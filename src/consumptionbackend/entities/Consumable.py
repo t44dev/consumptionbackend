8f7bf262b3b5382cac2b0672b60b5cbd20fa5f5a
t44dev <trackfortyfour@proton.me>
2025-04-30 15:51:56 +1000

    post-init on Consumable to transform some types

--- src/consumptionbackend/entities/Consumable.py
+++ src/consumptionbackend/entities/Consumable.py
@@ -19,3 +19,10 @@ class Consumable(EntityBase):
     rating: float | None
     start_date: datetime | None
     end_date: datetime | None
+
+    def __post_init__(self):
+        self.status = Status(self.status)
+        if isinstance(self.start_date, (float, int)):
+            self.start_date = datetime.fromtimestamp(self.start_date)
+        if isinstance(self.end_date, (float, int)):
+            self.end_date = datetime.fromtimestamp(self.end_date)
