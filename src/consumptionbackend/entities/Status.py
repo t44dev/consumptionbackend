0ef26a07efa3671f636063efc310c1b89427f6f5
t44dev <trackfortyfour@proton.me>
2025-12-06 15:19:58 +1100

    display string for Status

--- src/consumptionbackend/entities/Status.py
+++ src/consumptionbackend/entities/Status.py
@@ -1,4 +1,5 @@
 from enum import IntEnum
+from typing import override
 
 
 class Status(IntEnum):
@@ -7,3 +8,7 @@ class Status(IntEnum):
     ON_HOLD = 2
     DROPPED = 3
     COMPLETED = 4
+
+    @override
+    def __str__(self) -> str:
+        return self.name.replace("_", " ")
